d669e4e3cf71cae94839b850624e33884bd4ebb1
MisTurtle <mister.turtle490@gmail.com>
2023-05-12 18:12:13 +0200

    Rotating aruco markers to match the top left corner

--- ArucoCrop/ArucoArea.py
+++ ArucoCrop/ArucoArea.py
@@ -1,4 +1,9 @@
+import math
 from abc import abstractmethod, ABC
+from collections import Counter
+
+import cv2
+import numpy as np
 
 from ArucoCrop.ArucoCropUtils import rotate_img, get_center_points
 
@@ -8,7 +13,29 @@ class ArucoArea(ABC):
 	@staticmethod
 	def rotate_and_crop(image, rel_corners):
 		center_points = get_center_points(rel_corners)
-		return rotate_img(image, center_points)
+		straight = rotate_img(image, center_points)
+		top_left_corner_ids = []
+		for corners in rel_corners:
+			corners = corners.reshape(-1, 2).astype(np.int0)
+			minLen, topLeftId = float('inf'), 0
+			for cid, corner in enumerate(corners):
+				length = math.sqrt(corner[0] ** 2 + corner[1] ** 2)
+				if length < minLen:
+					minLen = length
+					topLeftId = cid
+			top_left_corner_ids.append(topLeftId)
+		counter = Counter(top_left_corner_ids)
+		common = counter.most_common(1)[0]
+		match common:
+			case 0:  # Top left for most markers is already their top left
+				return straight
+			case 1:  # Top left is mostly top right
+				angle = cv2.ROTATE_90_COUNTERCLOCKWISE
+			case 2:  # Top left is mostly bottom right
+				angle = cv2.ROTATE_180
+			case _:
+				angle = cv2.ROTATE_90_CLOCKWISE
+		return cv2.rotate(straight, angle)
 
 	def __init__(self, name: str, aruco_id: int, marker_count: int):
 		self.name = name
